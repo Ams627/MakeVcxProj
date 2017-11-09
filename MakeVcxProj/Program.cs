875a98373b6cbb8b36de6ac007ff4637baa4673b
Adrian Sims <asims@parkeon.com>
2017-11-09 09:38:03 +0000

    First commit

---
+++ MakeVcxProj/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeVcxProj
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
